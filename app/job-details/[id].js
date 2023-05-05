c1264f4a380c500e5d8acc83458d303f49dc0e42
Razaul00003 <mdrazaulkarim00003@gmail.com>
2023-05-05 22:45:21 +0600

    'added qualiticion tab content'

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -33,6 +33,34 @@ const JobDetails = () => {
     job_id: params.id,
   });
 
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+
+      case "About":
+        return (
+          <JobAbout info={data[0].job_description ?? "No data provided"} />
+        );
+
+      case "Responsibilities":
+        return (
+          <Specifics
+            title="Responsibilities"
+            points={data[0].job_highlights?.Responsibilities ?? ["N/A"]}
+          />
+        );
+
+      default:
+        return null;
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -82,6 +110,7 @@ const JobDetails = () => {
               activeTab={activeTab}
               setActiveTab={setActiveTab}
             />
+            {displayTabContent()}
           </View>
         )}
       </ScrollView>
